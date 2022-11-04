4bfd60d0cfa97e1aea08d40b3a2b95e32a114136
Jonathan Pierre-Louis <jopierrelouis@gmail.com>
2022-11-04 15:48:40 -0400

    fixed app.module.ts

--- DatingAPPFrontend/src/app/app.module.ts
+++ DatingAPPFrontend/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { LoginComponentComponent } from './components/login-component/login-component.component';
 import { HomepageComponentComponent } from './components/homepage-component/homepage-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     LoginComponentComponent,
     HomepageComponentComponent
   ],
