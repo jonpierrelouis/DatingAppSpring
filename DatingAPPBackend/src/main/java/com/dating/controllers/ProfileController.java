6234d52b8a40cfd9604b5422d7b1ae065604dd65
Jonathan Pierre-Louis <jopierrelouis@gmail.com>
2023-01-05 17:06:24 -0500

    added profile endpoints

--- DatingAPPBackend/src/main/java/com/dating/controllers/ProfileController.java
+++ DatingAPPBackend/src/main/java/com/dating/controllers/ProfileController.java
@@ -54,4 +54,96 @@ public class ProfileController {
 		return ResponseEntity.ok(optionalProfile.get());
 	}
 
+	/**
+	 * Endpoint to change the user's name
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/name")
+	public ResponseEntity<Profile> changeName(HttpSession session, HttpServletRequest req){
+		
+		Object userId = session.getAttribute("userId");
+		String newName = req.getParameter("newName");
+		
+		return ResponseEntity.ok(ps.changeName((Integer) userId, newName));
+	}
+	
+	/**
+	 * Endpoint to change the user's date of birth
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/dob")
+	public ResponseEntity<Profile> changeDOB(HttpSession session, HttpServletRequest req){
+		
+		Object userId = session.getAttribute("userId");
+		int day = Integer.parseInt(req.getParameter("newDay"));
+		int month = Integer.parseInt(req.getParameter("newMonth"));
+		int year = Integer.parseInt(req.getParameter("newYear"));
+		
+		return ResponseEntity.ok(ps.changeDOB((Integer) userId, day, month, year));
+	}
+	
+	/**
+	 * Endpoint to change the user's location
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/location")
+	public ResponseEntity<Profile> changeLocation(HttpSession session, HttpServletRequest req){
+		
+		Object userId = session.getAttribute("userId");
+		String city = req.getParameter("newCity");
+		String state = req.getParameter("newState");
+		
+		return ResponseEntity.ok(ps.changeLocation((Integer) userId, city, state));
+	}
+
+	/**
+	 * Endpoint to change the user's about yourself description
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/aboutyourself")
+	public ResponseEntity<Profile> changeAboutYourself(HttpSession session, HttpServletRequest req){
+	
+		Object userId = session.getAttribute("userId");
+		String aboutYourself = req.getParameter("newAboutYourself");
+		
+		return ResponseEntity.ok(ps.changeAboutYourself((Integer) userId, aboutYourself));
+	}
+
+	/**
+	 * Endpoint to change the user's gender
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/gender")
+	public ResponseEntity<Profile> changeGender(HttpSession session, HttpServletRequest req){
+	
+		Object userId = session.getAttribute("userId");
+		String gender = req.getParameter("newGender");
+		
+		return ResponseEntity.ok(ps.changeGender((Integer) userId, gender));
+	}
+
+	/**
+	 * Endpoint to change the user's sexual orientation
+	 * @param session
+	 * @param req
+	 * @return
+	 */
+	@PostMapping("edit/orientation")
+	public ResponseEntity<Profile> changeSexOriention(HttpSession session, HttpServletRequest req){
+	
+		Object userId = session.getAttribute("userId");
+		String orientation = req.getParameter("newOrientation");
+		
+		return ResponseEntity.ok(ps.changeSexOrientation((Integer) userId, orientation));
+	}
 }
